9a0e76aed516fe937152703f83b22f0630d63378
Line Båserud <lineb@met.no>
2021-06-10 15:21:58 +0200

    updated examples and added flexibility

--- getfrost_obs.R
+++ getfrost_obs.R
@@ -1,10 +1,11 @@
-#!/usr/bin/env Rscript 
 
 # Function: get observations from frost.met.no
 #
-# Example: getfrost_obs(18700, "2018-01-01","2018-02-02","max(air_temperature P1D)","P1D","PT18H")
+# Example: 
+# getfrost_obs("SN18700", "2018-01-01","2018-02-02","max(air_temperature P1D)","P1D","PT18H")
+# getfrost_obs("SN18700", "2018-01-01","2018-02-02","wind_speed","PT1H")
 #
-# NB: add your client_id to a file named client_id.txt (see client_id_dummy.txt for format)
+# NB: add client_id to a file named client_id.txt 
 
 # -------------------------------------------------------------------------------------------------
 
@@ -12,34 +13,36 @@ library(jsonlite)
 library(tidyr)
 library(readr)
 
-source("~/projects/frost/R_scripts/getfrost/err_messages.R")
+source("err_messages.R")
 
 # -------------------------------------------------------------------------------------------------
 
-# get observations from Frost
-getfrost_obs <- function(station, start, stop, element, t_resolution, t_offset){
+getfrost_obs <- function(station, start, stop, element, t_resolution, t_offset = NA, levels = NA){
   
   client_id  <- as.character(read_tsv("client_id.txt", col_names = TRUE, cols(client_id = col_character())))
   url <- paste0("https://", client_id, "@frost.met.no/observations/v0.jsonld?",
-               "sources=SN", station,
+               "sources=", station,
                "&referencetime=", start, "T00:00/", stop, "T23:59",
                "&elements=", element,
-               "&timeoffsets=", t_offset,
-               "&timeresolutions=", t_resolution,
-               "&includeextra=",1)
+               "&timeresolutions=", t_resolution)
+  if(!is.na(t_offset)){url <- paste0(url, "&timeoffsets=", t_offset)}
+  if(!is.na(levels)){url <- paste0(url, "&levels=", levels)}
 
   cat("------------------------------------------------------------------\n")
+
   output <- try(fromJSON(URLencode(url), flatten = TRUE))
+
   if (class(output) != 'try-error') {
-    print(paste0("SN", station, ": observations retrieved from frost.met.no! (", object.size(output), " bytes)"))
+    print(paste0(station, ": observations retrieved from frost.met.no! (", object.size(output), " bytes)"))
+
+    obs <- unnest(output$data, cols = c(observations))
+    obs$referenceTime <- as.POSIXct(strptime(x = as.character(obs$referenceTime), format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")) # transform date format  
+    return(obs)
+
   } else {
     err_messages()
   }
   
-  obs <- unnest(output$data, cols = c(observations))
-  obs$referenceTime <- as.POSIXct(strptime(x = as.character(obs$referenceTime), format = "%Y-%m-%dT%H:%M:%S", tz = "UTC")) # transform date format  
-  
-  return(obs)
 }
 
 
