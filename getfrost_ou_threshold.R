9dbd2e40109c8cdde304dcfe109c43c938a3bf48
Line Båserud <lineb@met.no>
2021-06-10 16:45:40 +0200

    new name and bug fix

--- getfrost_ou_threshold.R
+++ getfrost_ou_threshold.R
@@ -5,9 +5,9 @@
 # lineb@met.no
 #
 # Example: 
-# ou_threshold("above", 25, "SN18700", "2016-01-01","2018-12-31", "max(air_temperature P1D)", "P1D", "PT18H")
-# ou_threshold("below", 10, "SN18700", "2005-01-01","2020-12-31", "mean(air_temperature P1M)", "P1M", "PT0H")
-# ou_threshold("above", 20.8, "SN52535", "2020-01-01","2020-12-31", "max(wind_speed PT1H)", "PT1H", "PT0H")
+# getfrost_ou_threshold("over", 25, "SN18700", "2016-01-01","2018-12-31", "max(air_temperature P1D)", "P1D", "PT18H")
+# getfrost_ou_threshold("under", 10, "SN18700", "2005-01-01","2020-12-31", "mean(air_temperature P1M)", "P1M", "PT0H")
+# getfrost_ou_threshold("over", 20.8, "SN52535", "2020-01-01","2020-12-31", "max(wind_speed PT1H)", "PT1H", "PT0H")
 #
 # NB: add client_id to a file named client_id.txt 
 # note: add if no rows, message no data above/below
@@ -17,16 +17,16 @@ source("getfrost_obs.R")
 
 # -------------------------------------------------------------------------------------------------
 
-ou_threshold <- function(above_below, threshold, station, start, stop, element, t_resolution, t_offset){
+getfrost_ou_threshold <- function(over_under, threshold, station, start, stop, element, t_resolution, t_offset){
   
   df <- getfrost_obs(station, start, stop, element, t_resolution, t_offset)
 
-  if (above_below == "above"){
+  if (over_under == "over"){
     dates  <- df$referenceTime[which(df$value >= threshold)]
     values <- df$value[which(df$value >= threshold)]
-  } else if (above_below == "below"){
-    dates  <- df$referenceTime[which(df$value >= threshold)]
-    values <- df$value[which(df$value >= threshold)]
+  } else if (over_under == "under"){
+    dates  <- df$referenceTime[which(df$value <= threshold)]
+    values <- df$value[which(df$value <= threshold)]
   }
 
   len <- length(dates)
