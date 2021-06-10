370185ed04bf8d59a698563aeebcee9f459297a0
Line Båserud <lineb@met.no>
2021-06-10 15:38:55 +0200

    clean up

--- getfrost_stations.R
+++ getfrost_stations.R
@@ -1,24 +1,31 @@
-#!/usr/bin/env Rscript
 
 # Function: get dataframe with all stations in either municipality, county or country
 #
-# lineb@met.no 
 # 25 Aug 2020
+# lineb@met.no 
 #
 # Examples:
 # df <- getfrost_stations("county", "TROMS OG FINNMARK")
 # df <- getfrost_stations("municipality", "TROMSØ")
 # df <- getfrost_stations("country", "NORGE")
+# 
+# NB: add your client_id to a file named client_id.txt
+
+# -----------------------------------------------------------------------------------
+
+library(jsonlite)
+library(tidyr)
+library(readr)
 
 # -----------------------------------------------------------------------------------
 
 getfrost_stations <- function(area_type, area){
-
-  client_id  <- "ca076a66-7ea1-4ca2-8eaf-f091899a9eba:f8413549-8f24-4ce1-8154-e5a2c85e553c"
-  url <- paste0("https://", client_id, "@frost.met.no/sources/v0.jsonld?",
-               area_type,"=", area)
-  output <- try(fromJSON(URLencode(url),flatten=T))
-  df<-unnest(output$data,cols=c())
+  
+  client_id  <- as.character(read_tsv("client_id.txt", col_names = TRUE, cols(client_id = col_character())))
+  
+  url <- paste0("https://", client_id, "@frost.met.no/sources/v0.jsonld?", area_type, "=", area)
+  output <- try(fromJSON(URLencode(url), flatten = TRUE))
+  df <- unnest(output$data, cols = c())
 
   return(df)
 }
