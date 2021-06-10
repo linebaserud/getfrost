51d8a2cf91044dd920b048078a69557fbe03bbd3
Line Båserud <lineb@met.no>
2021-06-10 15:31:43 +0200

    updated examples and clean up

--- getfrost_meta.R
+++ getfrost_meta.R
@@ -1,9 +1,10 @@
-#!/usr/bin/env Rscript 
 
-# Function: retrieve meta data for specific station from frost.met.no
-#
-# Example: getfrost_meta(18700)
-#
+# Function: get meta data for stations from frost.met.no
+
+# Examples: 
+# Oslo Blindern and Bergen Florida: getfrost_meta("SN18700, SN50540")
+# All stations: getfrost_meta("") 
+# 
 # 30 September 2020 
 # lineb@met.no
 #
@@ -22,16 +23,15 @@ getfrost_meta <- function(station){
 
   client_id  <- as.character(read_tsv("client_id.txt", col_names = TRUE, cols(client_id = col_character())))
   url <- paste0("https://", client_id, "@frost.met.no/sources/v0.jsonld?",
-                "ids=SN", station)
+                "ids=", station)
 
   output <- try(fromJSON(URLencode(url), flatten = TRUE))
   if (class(output) != 'try-error') {
-    print(paste0("SN", station, ": metadata retrieved from frost.met.no! (", object.size(output), " bytes)"))
+    print(paste0(station, ": metadata retrieved from frost.met.no! (", object.size(output), " bytes)"))
+    
+    meta <- unnest(output$data, cols=c())
+    return(meta)
   } else {
     err_messages()
   }
-
-  meta <- unnest(output$data, cols=c())
-
-  return(meta)
 }
